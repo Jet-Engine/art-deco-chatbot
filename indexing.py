cfabd6047a3602475bcdc04304b58e8a94a9c09e
Guvenc Usanmaz <gusanmaz@gmail.com>
2024-05-09 12:25:43 +0300

    Update README, Migrate to litellm fully

--- indexing.py
+++ indexing.py
@@ -1,16 +1,30 @@
-import ollama, chromadb, time, os
+import chromadb, time, os, logging
 from utilities import read_text, get_config
 from mattsollamatools import chunk_text_by_sentences
 from tqdm import tqdm
+from litellm import embedding
+import nltk
+
+nltk.download('punkt')
+
+# Setup logging
+log_filename = os.path.splitext(os.path.basename(__file__))[0] + '.log'
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler(log_filename),
+                        logging.StreamHandler()
+                    ])
+
+logger = logging.getLogger()
 
 
 def create_collection(host, port, collection_name, delete_existing=True):
     client = chromadb.HttpClient(host=host, port=port)
     existing_collections = client.list_collections()
-    print(existing_collections)
+    logger.info(existing_collections)
     # Check if collection exists and delete if specified
     if delete_existing and any(collection.name == collection_name for collection in existing_collections):
-        print(f'Deleting collection: {collection_name}')
+        logger.info(f'Deleting collection: {collection_name}')
         client.delete_collection(collection_name)
     # Create or get the collection
     return client.get_or_create_collection(name=collection_name, metadata={"hnsw:space": "cosine"})
@@ -22,7 +36,7 @@ def add_text_file_to_collection(collection, file_path, embed_model, sentence_per
     chunks = chunk_text_by_sentences(source_text=text, sentences_per_chunk=sentence_per_chunk_val, overlap=overlap_val)
     pbar.set_description(f"Processing {filename}")
     for index, chunk in enumerate(tqdm(chunks, desc="Chunking", leave=False, position=1)):
-        embed = ollama.embeddings(model=embed_model, prompt=chunk)['embedding']
+        embed = embedding(model="ollama/" + embed_model, input=chunk)['data'][0]['embedding']
         collection.add([filename + str(index)], [embed], documents=[chunk], metadatas={"source": filename})
         pbar.update(1)  # Update the primary progress bar per chunk processed
 
@@ -53,7 +67,8 @@ def main():
         for file_path in [os.path.join(texts_path, f) for f in os.listdir(texts_path) if f.endswith(file_extension)]:
             add_text_file_to_collection(collection, file_path, embed_model, sentence_per_chunk_val, overlap_val, pbar)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    elapsed_time = time.time() - start_time
+    logger.info(f"--- {elapsed_time} seconds ---")
 
 
 if __name__ == "__main__":
